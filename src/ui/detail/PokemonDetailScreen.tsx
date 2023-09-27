f01aace72e22313dedcde84270d994e1ce3af94f
x1210x <x1210x@gmail.com>
2023-09-27 11:13:20 +0900

    순차로딩

--- src/ui/detail/PokemonDetailScreen.tsx
+++ src/ui/detail/PokemonDetailScreen.tsx
@@ -7,12 +7,12 @@ import {
     Text,
     View
 } from 'react-native';
-import {useRecoilState, useRecoilValueLoadable} from 'recoil';
+import PokemonRepository from '../../repository/PokemonRepository';
+import NetworkPokemonRepository from '../../repository/NetworkPokemonRepository';
+import PokemonDetail from './../model/PokemonDetail';
 import MyImage from '../common/MyImage';
 import EvolutionRow from '../common/EvolutionRow';
 import Utils from '../../util/Utils.tsx';
-import pokemonDetailParams from './PokemonDetailParams';
-import pokemonDetailViewModel from './PokemonDetailViewModel';
 
 const styles = StyleSheet.create({
     item: {
@@ -36,74 +36,116 @@ const styles = StyleSheet.create({
 });
 
 const PokemonDetailScreen = ({navigation, route}) => {
-    const [params, setParams] = useRecoilState(pokemonDetailParams);
-    const result = useRecoilValueLoadable(pokemonDetailViewModel(route.params.id));
-    console.debug(`PokemonDetailScreen(${result.state}) id = ${route.params.id}`);
+    const [pokemon, setPokemon] = useState(new PokemonDetail(
+        route.params.id,
+        route.params.name
+    ));
+
+    const getPokemonDetail = async () => {
+        const repository: PokemonRepository = NetworkPokemonRepository.getInstance();
+        // 상세
+        const pokemonDetail = await repository.getPokemonDetail(route.params.id);
+        setPokemon(new PokemonDetail(
+            route.params.id,
+            route.params.name,
+            pokemonDetail.height,
+            pokemonDetail.weight,
+        ));
+        // species
+        const species = await repository.getSpecies(pokemonDetail.sId);
+        setPokemon(new PokemonDetail(
+            route.params.id,
+            route.params.name,
+            pokemonDetail.height,
+            pokemonDetail.weight,
+            pokemonDetail.sId,
+            species,
+        ));
+        // form
+        const form: Form = await repository.getForm(pokemonDetail.fId);
+        setPokemon(new PokemonDetail(
+            route.params.id,
+            route.params.name,
+            pokemonDetail.height,
+            pokemonDetail.weight,
+            pokemonDetail.sId,
+            species,
+            pokemonDetail.fId,
+            form
+        ));
+        // type
+        const types = await Promise.all(pokemonDetail.tIds.map(async (tId) => {
+            const type = await repository.getType(tId);
+            console.debug(`${tId} type = ${type.name}`);
+            return type;
+        }));
+        setPokemon(new PokemonDetail(
+            route.params.id,
+            route.params.name,
+            pokemonDetail.height,
+            pokemonDetail.weight,
+            pokemonDetail.sId,
+            species,
+            pokemonDetail.fId,
+            form,
+            pokemonDetail.tIds,
+            types
+        ));
+        // evolution chain
+        const evolutionChain = await repository.getEvolutionChain(species.ecId);
+        setPokemon(new PokemonDetail(
+            route.params.id,
+            route.params.name,
+            pokemonDetail.height,
+            pokemonDetail.weight,
+            pokemonDetail.sId,
+            species,
+            pokemonDetail.fId,
+            form,
+            pokemonDetail.tIds,
+            types,
+            evolutionChain
+        ));
+    }
 
     useEffect(() => {
-        setParams(route.params.id);
+        getPokemonDetail();
     }, []);
 
-    switch(result.state) {
-        case 'loading':
-            {/* 기본이름 보여주고, flavor 영역에 프로그레스 돌림 */}
-            return (
-                <ScrollView style={{flex: 1, backgroundColor: 'white'}}>
-                    <View style={styles.item}>
-                        <MyImage
-                            style={{width: 200, aspectRatio: '1/1'}}
-                            source={{uri: Utils.getImageUrl(route.params.id)}}
-                        />
-                    </View>
-                    <View style={styles.item}>
-                        <Text style={{color: 'black'}}>{route.params.id} {route.params.name}</Text>
-                    </View>
-                    <View
-                        style={{flex: 1, alignContent: 'center', justifyContent: 'center'}}>
-                        <ActivityIndicator size='large' />
-                    </View>
-                </ScrollView>
-            );
-        case 'hasValue':
-            return (
-                <ScrollView style={{flex: 1, backgroundColor: 'white'}}>
-                    <View style={styles.item}>
-                        <MyImage
-                            style={{width: 200, aspectRatio: '1/1'}}
-                            source={{uri: Utils.getImageUrl(route.params.id)}}
-                        />
-                    </View>
-                    <View style={styles.item}>
-                        <Text style={{color: 'black', fontSize: result.contents.form.formName != undefined ? 20 : 30}}>
-                            {route.params.id} {result.contents.species.name} {result.contents.form.formName != undefined ? `(${result.contents.form.formName})`: ''}
-                        </Text>
-                    </View>
-                    <View style={[styles.item, styles.stat]}>
-                        <Text style={{color: 'black', fontSize: 12}}>
-                            몸무게: {(result.contents.weight / 10).toFixed(1)}kg
-                        </Text>
-                        <Text style={{color: 'black', fontSize: 12}}>
-                            키: {(result.contents.height / 10).toFixed(1)}m
-                        </Text>
-                        <Text style={{color: 'black', fontSize: 12}}>
-                            타입: {result.contents.types.map((type) => type.name).reduce((acc, cur) => `${acc}, ${cur}`)}
-                        </Text>
-                    </View>
-                    <View style={styles.item}>
-                        <Text style={{color: 'black', fontSize: 16}}>
-                            {result.contents.species.flavor.replace('\n', ' ')}
-                        </Text>
-                    </View>
-                    {result.contents.evolutionChain.pairs.map((pair) => (
-                        <EvolutionRow key={`${pair.from.id}-${pair.to.id}`} navigation={navigation} pId={route.params.id} pair={pair} />
-                    ))}
-                </ScrollView>
-            );
-        case 'hasError':
-            return (<Text>Error...</Text>);
-        default:
-            return (<Text>XXX</Text>);
-    }
+    return (
+        <ScrollView style={{flex: 1, backgroundColor: 'white'}}>
+            <View style={styles.item}>
+                <MyImage
+                    style={{width: 200, aspectRatio: '1/1'}}
+                    source={{uri: Utils.getImageUrl(route.params.id)}}
+                />
+            </View>
+            <View style={styles.item}>
+                <Text style={{color: 'black', fontSize: (pokemon.form != undefined && pokemon.form.formName != undefined) ? 20 : 30}}>
+                    {route.params.id} {pokemon.species != undefined ? pokemon.species.name : route.params.name} {(pokemon.form != undefined && pokemon.form.formName != undefined) ? `(${pokemon.form.formName})`: ''}
+                </Text>
+            </View>
+            <View style={[styles.item, styles.stat]}>
+                <Text style={{color: 'black', fontSize: 12}}>
+                    몸무게: {(pokemon.weight / 10).toFixed(1)}kg
+                </Text>
+                <Text style={{color: 'black', fontSize: 12}}>
+                    키: {(pokemon.height / 10).toFixed(1)}m
+                </Text>
+                <Text style={{color: 'black', fontSize: 12}}>
+                    타입: {pokemon.types != undefined ? pokemon.types.map((type) => type.name).reduce((acc, cur) => `${acc}, ${cur}`) : ''}
+                </Text>
+            </View>
+            <View style={styles.item}>
+                <Text style={{color: 'black', fontSize: 16}}>
+                    {pokemon.species != undefined ? pokemon.species.flavor.replace('\n', ' ') : ''}
+                </Text>
+            </View>
+            {pokemon.evolutionChain != undefined ? pokemon.evolutionChain.pairs.map((pair) => (
+                <EvolutionRow key={`${pair.from.id}-${pair.to.id}`} navigation={navigation} pId={route.params.id} pair={pair} />
+            )) : <View />}
+        </ScrollView>
+    );
 }
 
 export default PokemonDetailScreen;
